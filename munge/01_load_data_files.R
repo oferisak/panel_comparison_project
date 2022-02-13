01c9e914a6d982af0d87cafacc7584e9cc198219
oferisak <doctoroots@gmail.com>
2022-02-13 10:46:10 +0200

    last commit before transition to gtr

--- munge/01_load_data_files.R
+++ munge/01_load_data_files.R
@@ -1,6 +1,6 @@
 # Load all the sources into one table
 load_source_dbs <- function() {
-  db_files<-list.files('./data/databases/')
+  db_files<-list.files('./data/databases/other_sources/')
   source_db<-NULL
   db_versions<-NULL
   for (db_file in db_files){
@@ -8,7 +8,7 @@ load_source_dbs <- function() {
     db_version<-db_file%>%stringr::str_match('\\d+-\\d+-\\d+')%>%as.character()
     db_versions<-db_versions%>%bind_rows(data.frame(source=db_name,db_creation_date=db_version))
     message(glue('Parsing {db_name}..'))
-    db_table<-readr::read_delim(glue('./data/databases/{db_file}'))
+    db_table<-readr::read_delim(glue('./data/databases/other_sources/{db_file}'))
     db_table$panel_id<-as.character(db_table$panel_id)
     source_db<-source_db%>%bind_rows(db_table)
   }
@@ -17,7 +17,6 @@ load_source_dbs <- function() {
 }  
 
 source_db<-load_source_dbs()
-
 # fix the source file
 cache("fixed_source_db", {
   # remove red genes from panelapp  
@@ -44,5 +43,11 @@ get_panels_list_from_source_db <- function(source_db) {
            panel_joined_name=glue('{source}|{panel_id}|{panel_name}'))
   return(panels_list)
 }
+
+gtr_db_file<-'./data/databases/gtr/gtr_panels_db_2022-02-13.csv.gz'
+gtr_db<-readr::read_delim(gtr_db_file)
+gtr_db<-gtr_db%>%mutate(panel_joined_name=glue('{source}|{panel_id}|{panel_name}'))
+gtr_panels_list_file<-'./data/databases/gtr/gtr_panels_list_2022-02-13.csv.gz'
+gtr_panels_list<-readr::read_delim(gtr_panels_list_file)
 
 panels_list<-get_panels_list_from_source_db(source_db)
